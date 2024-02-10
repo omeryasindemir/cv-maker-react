b4963a23f8490b395498990d47cb1aab51d960d0
Ömer Yasin (felX) Demir <141151575+omeryasindemir@users.noreply.github.com>
2024-02-10 13:26:05 +0300

    Update firebase.config.js

--- src/config/firebase.config.js
+++ src/config/firebase.config.js
@@ -4,12 +4,13 @@ import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_API_KEY,
-  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-  projectId: process.env.REACT_APP_PROJECTID,
-  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_APPID,
+  apiKey: "AIzaSyB-0cT9Jr_h874r0La0yIl40cpww-6lCY4",
+  authDomain: "cv-maker-v2-db596.firebaseapp.com",
+  projectId: "cv-maker-v2-db596",
+  storageBucket: "cv-maker-v2-db596.appspot.com",
+  messagingSenderId: "559182910195",
+  appId: "1:559182910195:web:f643926cd4769f2517dd86",
+  measurementId: "G-P932KX040C"
 };
 
 const app = getApps.length > 0 ? getApp() : initializeApp(firebaseConfig);
